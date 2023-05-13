761cfd51933146fdc086751a2efe4c5b17e6d8c0
Sitti Rahma <sirah.yh@gmail.com>
2023-05-13 15:30:37 +0800

    kriteria 4 dan perbaikan kriteria 3

--- album/handler.js
+++ album/handler.js
@@ -1,56 +1,155 @@
 class AlbumsHandler{
 
+    constructor(service, validator) {
+        this._service = service;        
+        this._validator = validator;
+
+        this.postAlbumHandler = this.postAlbumHandler.bind(this);
+        this.getAlbumByIdHandler = this.getAlbumByIdHandler.bind(this);
+        this.putAlbumByIdHandler = this.putAlbumByIdHandler.bind(this);
+        this.deleteAlbumByIdHandler = this.deleteAlbumByIdHandler.bind(this);
+    }
+
     async postAlbumHandler(request, h) {
+        try {
+            this._validator.validateAlbumPayload(request.payload);
+            const { name, year } = request.payload;
+            const albumId = await this._service.addNote({ name, year });
         
-        const { name, year } = request.payload;
-    
-        const response = h.response({
-            status: 'success',
-            data: {
-                albumId : 'album_id'
-            },
-        });
-        response.code(201);
-        return response;
+            const response = h.response({
+                status: 'success',
+                data: {
+                    albumId : 'album_id'
+                },
+            });
+            response.code(201);
+            return response;
+        } catch (error) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                status: 'fail',
+                message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+        
+            // Server ERROR!
+            const response = h.response({
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
+            });
+            response.code(500);
+            console.error(error);
+            return response;
+        }
+        
     }
 
     async getAlbumByIdHandler(request, h) {
+        try {
+            const { id } = request.params;
+            const album = await this._service.getAlbumById(id);
         
-        const { id } = request.params;
-    
-        const response = h.response({
-            status: 'success',
-            data: {
-                album : album
-            },
-        });
-        response.code(200);
-        return response;
+            const response = h.response({
+                status: 'success',
+                data: {
+                    album : album
+                },
+            });
+            response.code(200);
+            return response;
+        } catch (error) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                status: 'fail',
+                message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+        
+            // Server ERROR!
+            const response = h.response({
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
+            });
+            response.code(500);
+            console.error(error);
+            return response;
+        }
+        
     }
 
     async putAlbumByIdHandler(request, h) {
+        try {
+            this._validator.validateAlbumPayload(request.payload);
+            const { id } = request.params;
+            const { name, year } = request.payload;
+
+            
+            await this._service.editAlbumById(id, { name, year });
         
-        const { id } = request.params;
-        const { name, year } = request.payload;
-    
-        const response = h.response({
-            status: 'success',
-            message: 'Album Berhasil diperbaharui',
-        });
-        response.code(200);
-        return response;
+            const response = h.response({
+                status: 'success',
+                message: 'Album Berhasil diperbaharui',
+            });
+            response.code(200);
+            return response;
+        } catch (error) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                status: 'fail',
+                message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+        
+            // Server ERROR!
+            const response = h.response({
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
+            });
+            response.code(500);
+            console.error(error);
+            return response;
+        }
+        
     }
 
     async deleteAlbumByIdHandler(request, h) {
+        try {
+            const { id } = request.params;
         
-        const { id } = request.params;
-    
-        const response = h.response({
-            status: 'success',
-            message: 'Album Berhasil dihapus',
-        });
-        response.code(200);
-        return response;
+            await this._service.deleteAlbumById(id);
+        
+            const response = h.response({
+                status: 'success',
+                message: 'Album Berhasil dihapus',
+            });
+            response.code(200);
+            return response;
+        } catch (error) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                status: 'fail',
+                message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+        
+            // Server ERROR!
+            const response = h.response({
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
+            });
+            response.code(500);
+            console.error(error);
+            return response;
+        }
+        
     }
 }
 
--- validator/schema.js
+++ validator/schema.js
@@ -3,7 +3,8 @@ const Joi = require('joi');
 const AlbumPayloadSchema = Joi.object({
     name: Joi.string().required(),
     year: Joi.number().required()
-})
+});
+
 const SongPayloadSchema = Joi.object({
   title: Joi.string().required(),
   year: Joi.number().required(),
